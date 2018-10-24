b3eb7b6aa8b43d58619e7168450a4198a2c099f6
Gabriellpa <gabriellpantunes@gmail.com>
2018-10-24 11:34:27 -0200

    ahaha

---
+++ Projeto2/Projeto2/wwwroot/js/JavaScript.js
@@ -0,0 +1,45 @@
+﻿TrelloPowerUp.initialize({
+    //BOARDS  
+    'board-buttons': function (t, options) {
+        return [{
+            text: 'Modal',
+
+            callback: function (t) {
+                return t.lists('all')
+                    .then(function (lists) {
+                        //console.log(JSON.stringify(lists, null, 2));
+                        let oDados = JSON.stringify(lists, null, 2);
+                        //for (let i = 0; i < oDados.length; i++) {
+                        //	section.push(oDados[i]);
+                        //}
+                        localStorage.setItem('listas', oDados);
+                        t.modal({
+                            // the url to load for the iframe
+                            url: '/Home/Modal',
+                            // optional arguments to be passed to the iframe as query parameters
+                            // access later with t.arg('text')
+                            args: { text: 'TesteModal' },
+                            // optional color for header chrome
+                            accentColor: '#0079BF',
+                            // initial height for iframe
+                            height: 1000, // not used if fullscreen is true
+                            // whether the modal should stretch to take up the whole screen
+                            fullscreen: false,
+                            // optional title for header chrome
+                            title: 'Atividades Board',
+                            // optional action buttons for header chrome
+                            // max 3, up to 1 on right side
+                            actions: [{
+                                icon: 'https://cdn.glitch.com/7fe26656-897b-4249-97d8-02357291b32b%2Ficon.svg.png?1536458755205',
+                                url: 'https://google.com',
+                                alt: 'Left',
+                                position: 'left'
+                            }],
+                        })
+                    });
+            }
+        }
+        ];
+    }
+}
+);
---
+++ Projeto2/Projeto2/wwwroot/js/modal.js
@@ -0,0 +1,23 @@
+﻿function popularCampos() {
+    var corpo;
+    var att;
+
+    corpo = document.getElementById('divModalAtiv');
+    var oListas = JSON.parse(localStorage.getItem('listas'));
+    var ul = newElement('ul');
+    for (let i = 0; i < oListas.length; i++) {
+        var element = newElement('li');
+        element.id = 'item' + i;
+        att = document.createAttribute("data-value");
+        att.value = oListas[i].id;
+        element.setAttributeNode(att);
+        element.innerHTML = oListas[i].name;
+        ul.append(element);
+    }
+    corpo.append(ul);
+}
+
+function newElement(tipo) {
+    var element = document.createElement(tipo);
+    return element;
+}
